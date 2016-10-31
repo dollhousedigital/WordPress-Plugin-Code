c31614fcc12ad9fb5efb0f9abf18c862b0e2a8b6
Brett Redvers <dollhousedigital@gmail.com>
2016-10-31 15:05:14 +0000

    Create Woo Password Strength Remove.php

---
+++ Woo Password Strength Remove.php
@@ -0,0 +1,6 @@
+function wc_ninja_remove_password_strength() {
+	if ( wp_script_is( 'wc-password-strength-meter', 'enqueued' ) ) {
+		wp_dequeue_script( 'wc-password-strength-meter' );
+	}
+}
+add_action( 'wp_print_scripts', 'wc_ninja_remove_password_strength', 100 );
